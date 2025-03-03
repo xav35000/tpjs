71acc26c2badbb70f78ce356791906c7957181a3
Boowie41 <160162527+Boowie41@users.noreply.github.com>
2025-03-03 15:28:52 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,26 +1,20 @@
 persDisplay = document.getElementById('personnes')
 quantDisplay = [...document.querySelectorAll('.quantite')]
-personnes = persDisplay.innerText
-quantites = quantDisplay.map(e => +e.innerText)
-
-console.log(quantites)
+personnes = +persDisplay.innerText
+quantites = quantDisplay.map(e => (+e.innerText) / personnes)
 
 function update() {
   persDisplay.innerText = personnes
 
   quantites.forEach((element, index) => {
-    quantDisplay[index].innerText = element.toString()
+    quantDisplay[index].innerText = (element * personnes).toString()
   })
 }
 
 
 function plus() {
   personnes += 1
+
+  
+  
 }
-
-quantites = quantites.map(e => e+1)
-personnes += 1
-
-console.log(quantites)
-
-update()
