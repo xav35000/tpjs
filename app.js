bb1aa0419853d3586af593ddca43aad27027dd63
Boowie41 <160162527+Boowie41@users.noreply.github.com>
2025-03-03 15:20:51 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 persDisplay = document.getElementById('personnes')
 quantDisplay = [...document.querySelectorAll('.quantite')]
 personnes = persDisplay.innerText
-quantites = quantDisplay.map(e => e.innerText)
+quantites = quantDisplay.map(e => +e.innerText)
 
 console.log(quantites)
 
 function update() {
   persDisplay.innerText = personnes
 
-  quantites.forEach((element) => {
-    element.inner
+  quantites.forEach((element, index) => {
+    quantDisplay[index].innerText = element.toString()
   })
   
 }
