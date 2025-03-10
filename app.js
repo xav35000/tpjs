7ab94240e3655da379b11981c7a301d7feba24e9
Boowie41 <160162527+Boowie41@users.noreply.github.com>
2025-03-10 15:10:07 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-persSelect = document.getElementById('nombre');
-persSelect.value = '4';
-quantDisplay = [...document.querySelectorAll('.quantite')];
-personnes = +persSelect.value;
-quantites = quantDisplay.map(e => (+e.innerText) / personnes);
+persSelect = document.getElementById('nombre'); // On récupère l'élément d'id "nombre" qui désigne le selecteur du nombre de personnes
+persSelect.value = '4'; // On s'assure que la valeur de la liste déroulante est à 4 personnes
+quantDisplay = [...document.querySelectorAll('.quantite')]; // On récupère tous les éléments de classe "quantité" (soit les span contenant les quantités) et on les stocke dans la liste quantDisplay
+personnes = +persSelect.value; // On récupère la valeur de persSelect qui correspond au nombre de personnes séléctionné, en le tranformant en nombre
+quantites = quantDisplay.map(e => (+e.innerText) / personnes); // On stocke les quantités pour 1 personne pour chaque ingrédient en divisant les quantités (transformés en nombre) par le nombre de personnes
 
-function update() {
-  personnes = +persSelect.value;
+function update() { // La fonction étant liée à l'événement 'onchange' du selecteur de personnes. Elle s'éxécutera donc à chaque de changement du nombre de personnes
+  personnes = +persSelect.value; // On récupère la nouvelle valeur du selecteur, et donc le nombre de personnes
 
   quantites.forEach((element, index) => {
-    quantDisplay[index].innerText = Math.ceil(element * personnes).toString()
-  })
+    quantDisplay[index].innerText = Math.ceil(element * personnes).toString();
+  });
 }
